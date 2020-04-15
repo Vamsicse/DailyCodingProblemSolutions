abe9b72def41a01dbdb213289c4edb30882d32f4
VAMSI KRISHNA MYALAPALLI <vamsikrishna.vasu@gmail.com>
2020-04-15 16:27:09 -0700

    Update SmallestPositiveNumber.java

--- solution/SmallestPositiveNumber.java
+++ solution/SmallestPositiveNumber.java
@@ -12,67 +12,40 @@ import java.util.Arrays;
  */
 public class SmallestPositiveNumber {
 
-    static int segregate(int arr[], int size)
-    {
-        int j = 0;
-        for (int i = 0; i < size; i++) {
-            if (arr[i] <= 0) {
-                int temp;
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                // increment count of non-positive integers
-                j++;
+    public static int firstMissingPositive(int[] nums, int size) {
+        for(int i=0 ;i < size; i++){
+            if(nums[i] <= 0 || nums[i] > size){
+                nums[i] = size+1;
             }
         }
-        return j;  // return count of non-positive integers
+        for(int i = 0; i < size; i++){
+            int pos = nums[i] > 0 ? nums[i] : (-1 * nums[i] );
+            if(pos <= size && nums[pos-1] > 0){
+                nums[pos-1] *= -1;
+            }
+        }
+        for(int i = 0 ; i < size; i++){
+            if(nums[i] > 0 ){
+                return i+1;
+            }
+        }
+        return size+1;
     }
 
-    /* Find smallest positive missing number that contains all positive integers */
-    static int findMissingPositive(int arr[], int size)
-    {
-        int i;
-        // Mark arr[i] as visited by making arr[arr[i] - 1] negative.
-        // Note that 1 is subtracted because index start from 0 and positive numbers start from 1
-        for (i = 0; i < size; i++) {
-            int x = Math.abs(arr[i]);
-            if (x - 1 < size && arr[x - 1] > 0) {
-                arr[x - 1] = -arr[x - 1];
-            }
-        }
-        // Return the first index value at which is positive
-        for (i = 0; i < size; i++) {
-            if (arr[i] > 0) {
-                return i + 1; // 1 is added because indexes start from 0
-            }
-        }
-        return size + 1;
-    }
-
-    /* Find smallest positive missing number that contains both positive and negative integers */
-    static int findMissing(int arr[], int size)
-    {
-        // First separate positive and negative numbers
-        int shift = segregate(arr, size);
-        int arr2[] = new int[size - shift];
-        int j = 0;
-        for (int i = shift; i < size; i++) {
-            arr2[j] = arr[i];
-            j++;
-        }
-        // Shift the array and call findMissingPositive for positive part
-        return findMissingPositive(arr2, j);
-    }
 
     public static void main(String[] args) {
         int arr1[] = {0, 10, 2, -10, -20};
-        System.out.println("Missing smallest positive number among " + Arrays.toString(arr1) + " is: " + findMissing(arr1, arr1.length));
+        System.out.println("Missing smallest positive number among " + Arrays.toString(arr1) + " is: ");
+        System.out.println(firstMissingPositive(arr1, arr1.length));
         int arr2[] = {3, 4, -1, 1};
-        System.out.println("Missing smallest positive number among " + Arrays.toString(arr2) + " is: " + findMissing(arr2, arr2.length));
+        System.out.println("Missing smallest positive number among " + Arrays.toString(arr2) + " is: ");
+        System.out.println(firstMissingPositive(arr2, arr2.length));
         int arr3[] = {1, 2, 0};
-        System.out.println("Missing smallest positive number among " + Arrays.toString(arr3) + " is: " + findMissing(arr3, arr3.length));
-        int arr4[] = {1, 2, 3, 10};
-        System.out.println("Missing smallest positive number among " + Arrays.toString(arr4) + " is: " + findMissing(arr4, arr4.length));
+        System.out.println("Missing smallest positive number among " + Arrays.toString(arr3) + " is: ");
+        System.out.println(firstMissingPositive(arr3, arr3.length));
+        int arr4[] = {-1, -2, -3, 10};
+        System.out.println("Missing smallest positive number among " + Arrays.toString(arr4) + " is: ");
+        System.out.println(firstMissingPositive(arr4, arr4.length));
     }
 
 }
