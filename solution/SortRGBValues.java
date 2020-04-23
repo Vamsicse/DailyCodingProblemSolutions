e6c818de6ea220822bb8b2888496b234ce4c4214
VAMSI KRISHNA MYALAPALLI <vamsikrishna.vasu@gmail.com>
2020-04-23 00:54:38 -0700

    Create SortRGBValues.java

---
+++ solution/SortRGBValues.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+/**
+ * Problem: Sort RGB Values in linear time.
+ * Approach: Use H
+ *
+ * Time complexity : O(n)
+ * Space complexity : In place sorting. Use an if case for each of R,G,B values.
+ *
+ * @author  Vamsi Krishna Myalapalli
+ * @since   2019-12-27
+ */
+public class SortRGBValues {
+
+    public static void main(String... args) {
+        char[] arr = { 'G', 'B', 'R', 'R', 'B', 'R', 'G' };
+        System.out.println("Before: " + Arrays.toString(arr));
+        sort(arr);
+        System.out.println("After:  " + Arrays.toString(arr));
+    }
+
+    public static void sort(char[] arr) {
+        int b = arr.length - 1;
+        for (int r=0, g=0; g <= b ; ) {
+            if (arr[g] == 'R') {
+                swap(arr, g, r);
+                r++;
+                g++;
+            }
+            else if (arr[g] == 'G') {
+                g++;
+            }
+            else {
+                swap(arr, g, b);
+                b--;
+            }
+        }
+    }
+
+    private static void swap(char[] arr, int i, int j) {
+        char ch = arr[i];
+        arr[i] = arr[j];
+        arr[j] = ch;
+    }
+
+}
+
+
+/*
+Output:
+—————————————————————— 
+
+Before: [G, B, R, R, B, R, G]
+After:  [R, R, R, G, G, B, B]
+
+
+*/
+
