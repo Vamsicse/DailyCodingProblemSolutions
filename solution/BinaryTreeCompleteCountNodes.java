61594b2f71ad4b4348e7a0ed34de46cefa92839d
VAMSI KRISHNA MYALAPALLI <vamsikrishna.vasu@gmail.com>
2020-06-17 13:40:45 -0700

    Update BinaryTreeCompleteCountNodes.java

--- solution/BinaryTreeCompleteCountNodes.java
+++ solution/BinaryTreeCompleteCountNodes.java
@@ -20,17 +20,43 @@ public class BinaryTreeCompleteCountNodes {
         }
     }
 
+    int leafCount = 0;
+
+    // Runtime: 0ms, Memory: 42MB
     public int countNodes(TreeNode root) {
-        return countNodes(root, 0);
+        if(root == null) {
+            return 0;
+        }
+
+        int height = findTreeHeight(root,0);
+        getLeafCount(root,height);
+        int count = 0;
+        for(int i=0; i<height; i++) {
+            count += Math.pow(2,i);
+        }
+        return count + leafCount;
     }
 
-    int countNodes(TreeNode root, int count){
-        if(root == null){
-            return count;
+    private void getLeafCount(TreeNode root, int depth) {
+        if(root != null) {
+            if(depth == 0) {
+                leafCount++;
+            }
+            else {
+                getLeafCount(root.left, depth-1);
+                getLeafCount(root.right, depth-1);
+            }
         }
-        ++count;
-        count = countNodes(root.left, count);
-        return countNodes(root.right, count);
+    }
+
+    private int findTreeHeight(TreeNode root, int height) {
+        if(root == null) {
+            return height;
+        }
+        if(root.left == null && root.right == null) {
+            return height;
+        }
+        return Math.max(findTreeHeight(root.left, height+1), findTreeHeight(root.right, height+1));
     }
 
     public static void main(String[] args) {
