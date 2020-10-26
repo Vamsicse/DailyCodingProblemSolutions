611fad106f1dca1a3901eeed1fd7ff2c1fd91619
VAMSI KRISHNA <vamsikrishna.vasu@gmail.com>
2020-10-26 13:37:40 -0700

    Update AngleBetweenClockHands.java

--- solution/AngleBetweenClockHands.java
+++ solution/AngleBetweenClockHands.java
@@ -1,6 +1,16 @@
 /**
  * Problem: Calculate Angle Between Clock Hands
- * Approach: Use formula
+ * Approach: Use formula.
+ * Explanation: Hour Hand
+                   In 12 hours Hour hand complete whole circle and cover 360°
+                   1 hour - 360° / 12 = 30°
+                Minute Hand
+                   In 60 minutes Minute Hand coplete whole circle and cover 360°.
+                   1 minute -> 360° / 60 = 6°
+
+                   1 hour - 60 minutes
+                   1 hours - 30°
+                   1 minute hours hand rotate -> 30° / 60 = 0.5°
  *
  * Time Complexity: O(1)
  * Space Complexity: O(1)
@@ -12,7 +22,7 @@ public class AngleBetweenClockHands {
 
     // Runtime: 0ms, Memory: 36.1MB
     public double angleClock(int hour, int minutes) {
-        double distance = Math.abs((hour % 12) * 30 - 5.5 * minutes);
+        double distance = Math.abs((hour % 12) * 30 - 5.5 * minutes);    // Math.abs(30.0 * hour + 0.5  * minutes - 6 * minutes)
         return Math.min(distance, 360 - distance);
     }
 
