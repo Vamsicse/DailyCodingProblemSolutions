c0f306e5cceaade656609c8d063a8d923bc5d5ae
Vamsi Krishna Myalapalli <v.myalapalli@walmart.com>
2021-02-23 00:36:34 -0800

    SearchInsertPosition

---
+++ solution/SearchInsertPosition.java
@@ -0,0 +1,54 @@
+/**
+ * Problem: Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ *          LeetCode: #35
+ * Approach: Use Binary Search
+ *
+ * Time Complexity: O(logn)
+ * Auxiliary Space: O(1)
+ *
+ * @author Vamsi Krishna Myalapalli
+ * @version 1.0
+ * @since 2020-12-27
+ */
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+        if(target< nums[0])
+            return 0;
+        if(target>nums[nums.length-1])
+            return nums.length;
+        int l=0, mid, h=nums.length-1;
+        while(l<nums.length && h<nums.length && l<=h) {
+            mid = l+(h-l)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(target<nums[mid])
+                h=mid-1;
+            else if(target>nums[mid])
+                l=mid+1;
+        }
+        if(nums[l]>target)
+            return l;
+        else
+            return l+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 5));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 2));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 7));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 0));
+    }
+
+}
+
+/*
+Output:
+——————————————————————
+
+2
+1
+4
+0
+
+*/
