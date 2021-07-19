6302bc32e87534c20dcbb93cd91bf4fb721b6cfb
vkrishna <vkrishna@atlassian.com>
2021-07-19 14:45:56 -0700

    FrequentSubtreeSum

---
+++ solution/FrequentSubtreeSum.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class FrequentSubtreeSum {
+
+    private static int highFreq = 0;
+
+    // Runtime: 2ms, Memory: 40MB
+    public static int[] findFrequentTreeSum(TreeNode root) {
+        List<Integer> list = new ArrayList();
+        HashMap<Integer,Integer> map = new HashMap();
+        dfs(root, list, map);
+        int [] res = new int[list.size()];
+        for(int i=0;i<list.size();i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    private static int dfs(TreeNode node, List<Integer> list, HashMap<Integer,Integer> map) {
+        if(node ==null)
+            return 0;
+        int left =  dfs(node.left, list, map);
+        int right =   dfs(node.right, list, map);
+
+        int sum = left+right + node.val;
+        map.put(sum,  map.getOrDefault(sum,0)+1);
+
+        if(map.get(sum)> highFreq)  {
+            list.clear();
+            list.add(sum);
+            highFreq = map.get(sum);
+        }
+        else if(map.get(sum)== highFreq) {
+            list.add(sum);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(5);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(-3);
+        System.out.println(Arrays.toString(findFrequentTreeSum(root1)));
+
+        TreeNode root2 = new TreeNode(5);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(-5);
+        System.out.println(Arrays.toString(findFrequentTreeSum(root2)));
+    }
+
+}
+
+/*
+Output:
+——————————————————————
+[2, -3, 4]
+[2]
+
+
+*/
