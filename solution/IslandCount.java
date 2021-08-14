47d552ce4440c088c1b31ac334e9bab5b82df228
VAMSI KRISHNA <vamsikrishna.vasu@gmail.com>
2021-08-14 13:41:10 -0700

    Add files via upload

---
+++ solution/IslandCount.java
@@ -0,0 +1,69 @@
+/**
+ * Problem: Find number of islands in 2D array, 1-Island, 0-Water
+ * Approach: Use DFS
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(amount). Memoization table.
+ *
+ * @author Vamsi Krishna Myalapalli
+ * @since 2019-12-27
+ */
+public class IslandCount {
+
+    public static void main(String... z){
+        char[][] grid1 = {  {'1', '1', '1', '1', '0'},
+                            {'1', '1', '0', '1', '0'},
+                            {'1', '1', '0', '0', '0'},
+                            {'0', '0', '0', '0', '0'}};
+        System.out.println("Islands: " + new IslandCount().numIslands(grid1));
+
+        char[][] grid2 = {{'1', '1', '0', '0', '0'},
+                          {'1', '1', '0', '0', '0'},
+                          {'0', '0', '1', '0', '0'},
+                          {'0', '0', '0', '1', '1'}};
+        System.out.println("Islands: " + new IslandCount().numIslands(grid2));
+
+        char[][] grid3 = {{'1', '0', '0', '0', '0'},
+                          {'0', '0', '1', '1', '0'},
+                          {'0', '1', '1', '0', '0'},
+                          {'0', '0', '0', '0', '0'},
+                          {'1', '1', '0', '0', '1'},
+                          {'1', '1', '0', '0', '1'}};
+        System.out.println("Islands: " + new IslandCount().numIslands(grid3));
+    }
+
+    // Runtime: 1ms, Memory: 41MB
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+
+    public static void dfs(char[][] m, int i, int j) {
+        if (!(i >= 0 && i < m.length && j >= 0 && j < m[0].length && m[i][j] == '1'))
+            return;
+        m[i][j] = '0';
+        dfs(m, i-1, j);
+        dfs(m, i+1, j);
+        dfs(m, i, j-1);
+        dfs(m, i, j+1);
+    }
+
+}
+
+
+/*
+Output:
+——————————————————————
+Islands: 1
+Islands: 3
+Islands: 4
+
+*/
